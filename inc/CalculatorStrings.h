634436ec8f6899a7fe8e203086b2c9cdd6b38bea
Jeff Trent <jeff.trent@intimetec.com>
2021-06-02 09:33:42 -0600

    Addressed feedback from Thomas

--- inc/CalculatorStrings.h
+++ inc/CalculatorStrings.h
@@ -19,63 +19,63 @@ namespace calculator
          * @brief ""
          * 
          */
-        static const std::string EMPTY_STRING;
+        static const char EMPTY_STRING[];
 
         /**
          * @brief " = "
          * 
          */
-        static const std::string EQUAL;
+        static const char EQUAL[];
 
         /**
          * @brief Beginning of error message
          * 
          */
-        static const std::string ERROR_MESSAGE;
+        static const char ERROR_MESSAGE[];
 
         /**
          * @brief appended to error message if divide by zero is attempted
          * 
          */
-        static const std::string ERROR_MESSAGE_DIVIDE_BY_ZERO;
+        static const char ERROR_MESSAGE_DIVIDE_BY_ZERO[];
 
         /**
          * @brief appended to error message if input is invalid
          * 
          */
-        static const std::string ERROR_MESSAGE_INVALID_INPUT;
+        static const char ERROR_MESSAGE_INVALID_INPUT[];
 
         /**
          * @brief word to quit the app
          * 
          */
-        static const std::string EXIT;
+        static const char EXIT[];
         
         /**
          * @brief response given to user if input is invalid
          * 
          */
-        static const std::string INVALID_INPUT;
+        static const char INVALID_INPUT[];
 
         /**
          * @brief " "
          * 
          */
-        static const std::string SPACE;
+        static const char SPACE[];
 
         /**
          * @brief prompt passed to user before retrieving input from them
          *        This prompt was used for the week 1 implementation of Calculator
          * 
          */
-        static const std::string W1_MAIN_PROMPT; 
+        static const char W1_MAIN_PROMPT[]; 
 
         /**
          * @brief prompt passed to user before retrieving input from them
          *        this prompt was used for the week 2 implementation of Calculator
          * 
          */
-        static const std::string W2_MAIN_PROMPT;
+        static const char W2_MAIN_PROMPT[];
     };
 } // namespace calculator
 
--- src/CalculatorStrings.cpp
+++ src/CalculatorStrings.cpp
@@ -11,14 +11,14 @@
 
 namespace calculator
 {
-    const std::string CalculatorStrings::EMPTY_STRING = "";
-    const std::string CalculatorStrings::EQUAL = " = ";
-    const std::string CalculatorStrings::ERROR_MESSAGE = "Error:";
-    const std::string CalculatorStrings::ERROR_MESSAGE_DIVIDE_BY_ZERO = " divide by zero";
-    const std::string CalculatorStrings::ERROR_MESSAGE_INVALID_INPUT = " invalid input";
-    const std::string CalculatorStrings::EXIT = "exit";
-    const std::string CalculatorStrings::INVALID_INPUT = "Invalid format please try again.";
-    const std::string CalculatorStrings::SPACE = " ";
-    const std::string CalculatorStrings::W1_MAIN_PROMPT = "Enter two numbers, e.g. '4 10':\n(ctrl-c to exit)";
-    const std::string CalculatorStrings::W2_MAIN_PROMPT = "Input a simple math expression e.g., '5 + 5'\nValid operations are [+-/*]\n(Enter 'exit' to quit)";
+    const char CalculatorStrings::EMPTY_STRING[] = "";
+    const char CalculatorStrings::EQUAL[] = " = ";
+    const char CalculatorStrings::ERROR_MESSAGE[] = "Error:";
+    const char CalculatorStrings::ERROR_MESSAGE_DIVIDE_BY_ZERO[] = " divide by zero";
+    const char CalculatorStrings::ERROR_MESSAGE_INVALID_INPUT[] = " invalid input";
+    const char CalculatorStrings::EXIT[] = "exit";
+    const char CalculatorStrings::INVALID_INPUT[] = "Invalid format please try again.";
+    const char CalculatorStrings::SPACE[] = " ";
+    const char CalculatorStrings::W1_MAIN_PROMPT[] = "Enter two numbers, e.g. '4 10':\n(ctrl-c to exit)";
+    const char CalculatorStrings::W2_MAIN_PROMPT[] = "Input a simple math expression e.g., '5 + 5'\nValid operations are [+-/*]\n(Enter 'exit' to quit)";
 } // namespace calculator
