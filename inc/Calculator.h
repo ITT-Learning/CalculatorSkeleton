1abd5d8ef7eab60bbac357e7a7b4138a34789a54
Gary Dean Jenkins <gary.jenkins@intimetec.com>
2021-07-14 15:49:47 -0600

    refactoring after conversations

--- inc/Calculator.h
+++ inc/Calculator.h
@@ -14,8 +14,7 @@
 
 //Method to call from main. Loops until error/state > 0
 void RunCalculator(); 
-
-//Parses input and outputs computed value
-int RenderCalculator(); 
+const char* GetInput(char& operation, double& a, double& b);
+const char* Calculate(char operation, double a, double b, double& result);
 
 #endif  // CALCULATOR_H
--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -8,53 +8,76 @@
 
 #include "Calculator.h"
 
+const char quitMessage[] = "Quitting!";
+const char unknownOperationError[] = "Unknwon operation!";
+
 void RunCalculator()
 {
     std::cout << "Gary's Calculator C++ learning project." << std::endl;
     std::cout << "Enter an expression, like 1 + 2 and ENTER. Valid operations are +-*/" << std::endl;
-    int state = 0;
-    double a = 0.0, b = 0.0;
 
-    while (state == 0)
-    {
-        state = RenderCalculator();
-    }
-}
-
-int RenderCalculator()
-{
-    int state = 0;
     double a, b, result;
     char operation;
 
+    const char* errorMessage = GetInput(operation, a, b);
+
+    while (errorMessage == nullptr)
+    {
+        errorMessage = Calculate(operation, a, b, result);
+        if (errorMessage == nullptr)
+        {
+            std::cout << " = " << result << std::endl;
+            errorMessage = GetInput(operation, a, b);
+        }     
+    }
+
+    std::cout << errorMessage << std::endl;
+}
+
+const char* GetInput(char& operation, double& a, double& b) 
+{
+    const char* errorMessage = nullptr;
+    double result;
+
     std::cin >> a >> operation >> b;
+
+    if (operation == 'q')
+    {
+        errorMessage = quitMessage;
+    }
+
+    return errorMessage;
+}
+
+const char* Calculate(char operation, double a, double b, double& result)
+{
+    const char* errorMessage = nullptr;
 
     switch (operation)
     {
         case '+':
+        {
             result = a + b;
             break;
+        }
         case '-':
-            result = a - b;
+        {
+           result = a - b;
             break;
+        }
         case '*':
+        {
             result = a * b;
             break;
+        }
         case '/':
+        {
             result = a / b;
             break;
+        }
         default:
-            state = 1;
-    }
+            errorMessage = unknownOperationError;
+    };
 
-    if (0 == state) 
-    {
-        std::cout << " = " << result << std::endl;
-    }
-    else
-    {
-        std::cout << "Unkown Operation (" << operation << ")!" << std::endl;
-    }
-
-    return state;
-}
+    return errorMessage;
+};
