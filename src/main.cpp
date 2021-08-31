4da98054934a18c058efbbf9cb8724dac9392183
Marcus Antimie <marcus.antimie@intimetec.com>
2021-08-31 11:27:05 -0600

    fixed some changes

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,26 @@
 int main() 
 {
     //This calculator can do super basic and simple math. 
-
-<<<<<<< HEAD
-=======
-    Calculator calc;
-
-    std::cout << calc.hello() << std::endl;
->>>>>>> ef987d43c131cce18f53162efa2b96e782a97cf8
-
-    
-    float number1, number2;
-    std::cout << "Welcome to the Calculator C++ learning project." << std::endl;
-    std::cout << "Please enter in two numbers separated by a space" << std::endl;
-    std::cin >> number1 >> number2;
-    int addResult = number1 + number2;
-    int multiplyResult = number1 * number2;
-    float divideResult = number1 / number2;
-    int subtractResult = number1 - number2;
-    std::cout << number1 << " + " << number2 << " = " << addResult << std::endl; 
-    std::cout << number1 << " X " << number2 << " = " << multiplyResult << std::endl;
-    std::cout << number1 << " % " << number2 << " = " << divideResult << std::endl;
-    std::cout << number1 << " - " << number2 << " = " << subtractResult << std::endl;
-
-    
-
-    //here is the end of the program 
-
-<<<<<<< HEAD
-
-=======
-    //testing
->>>>>>> ef987d43c131cce18f53162efa2b96e782a97cf8
+    while(true)
+    {
+        char repeat; 
+        float number1, number2;
+        std::cout << "Welcome to the Calculator C++ learning project." << std::endl;
+        std::cout << "Please enter in two numbers separated by a space" << std::endl;
+        std::cin >> number1 >> number2;
+        int addResult = number1 + number2;
+        int multiplyResult = number1 * number2;
+        float divideResult = number1 / number2;
+        int subtractResult = number1 - number2;
+        std::cout << number1 << " + " << number2 << " = " << addResult << std::endl; 
+        std::cout << number1 << " X " << number2 << " = " << multiplyResult << std::endl;
+        std::cout << number1 << " % " << number2 << " = " << divideResult << std::endl;
+        std::cout << number1 << " - " << number2 << " = " << subtractResult << std::endl;
+        std::cout << "Would you like to run the program again? y/n" << std::endl;
+        std::cin >> repeat;
+        if(repeat != 'y')
+        {
+            return 0;
+        }
+    }
 }
