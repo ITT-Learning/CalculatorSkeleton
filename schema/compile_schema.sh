1b11349734ceedd638050ec1711e130b9de924cc
Jared VandeWater <jared.vandewater@intimetec.com>
2021-11-08 11:56:06 -0700

    removed bash unused bash file

--- schema/compile_schema.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "If this isn't working, make sure you run cmake and build everything again"
-
-./../third-party/flatbuffers/flatbuffers-build/flatc --cpp --gen-object-api --gen-mutable *.fbs
