89f97a241a3ec6cfed9a3efe738fbe0400980a41
Simon Swenson <simon.swenson@intimetec.com>
2022-03-17 13:29:59 -0600

    some work on the http server

--- CalculatorBackEnd/src/HttpServer/inc/HttpRequestHandler.h
+++ CalculatorBackEnd/src/HttpServer/inc/HttpRequestHandler.h
@@ -8,6 +8,9 @@
 #ifndef HTTPREQUESTHANDLER_H
 #define HTTPREQUESTHANDLER_H
 
+#include <mutex>
+
+#include <pistache/endpoint.h>
 #include <pistache/http.h>
 
 #include "ExpressionSerializer.h"
@@ -15,26 +18,45 @@
 namespace calculator { namespace httpserver
 {
 
-/**
- * @brief a class for handling HTTP requests of all types, from 
- */
-class HttpRequestHandler : public Pistache::Http::Handler
+template<typename T>
+class CalculatorEndPoint
 {
     public:
-        HTTP_PROTOTYPE(HttpRequestHandler)
-        void onRequest(const Pistache::Http::Request& req, Pistache::Http::ResponseWriter response) override;
+        CalculatorEndPoint(Pistache::Address address, size_t threads = 4);
+        void start();
     private:
-        static constexpr const char *API_PREFIX = "/api/";
-        static constexpr const char *API_LIST_PREFIX = "list";
-        static constexpr const char *API_STORE_PREFIX = "store";
-        static constexpr const char *API_CALCULATE_PREFIX = "calculate";
-        static constexpr const char *FILE_ROOT_DIRECTORY = "./dist";
+        static constexpr const char API_LIST_PREFIX[] = "/calculator/list";
+        static constexpr const char API_STORE_PREFIX[] = "/calculator/store";
+        static constexpr const char API_CALCULATE_PREFIX[] = "/calculator/calculate";
+        static constexpr const char FILE_RESOURCE_PREFIX[] = "/res";
+
+        static constexpr const char FILE_ROOT_DIRECTORY[] = "./dist";
+
+        // Not exactly sure why this endpoint is shared. It was shared in the Pistache example rest_server.cc, but it
+        // didn't seem to actually have a handle anywhere outside of the enclosing endpoint class.
+        std::shared_ptr<Pistache::Http::Endpoint> internalEndPoint_;
+        Pistache::Rest::Router router_;
+        std::mutex savedExpressionsLock_;
+        std::vector<calculator::expression::ExpressionSerializer<T>::ExpressionBuffer> savedExpressions_;
+
+        void initializeRoutes();
+        void initializeEndPoint(size_t threads);
+
+        void handleApiStore(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+        void handleApiList(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+        void handleApiCalculate(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
+        void handleFileResource(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response);
 };
-constexpr const char *HttpRequestHandler::API_PREFIX;
-constexpr const char *HttpRequestHandler::API_LIST_PREFIX;
-constexpr const char *HttpRequestHandler::API_STORE_PREFIX;
-constexpr const char *HttpRequestHandler::API_CALCULATE_PREFIX;
-constexpr const char *HttpRequestHandler::FILE_ROOT_DIRECTORY;
+template<typename T>
+constexpr const char CalculatorEndPoint<T>::API_LIST_PREFIX[];
+template<typename T>
+constexpr const char CalculatorEndPoint<T>::API_STORE_PREFIX[];
+template<typename T>
+constexpr const char CalculatorEndPoint<T>::API_CALCULATE_PREFIX[];
+template<typename T>
+constexpr const char CalculatorEndPoint<T>::FILE_RESOURCE_PREFIX[];
+template<typename T>
+constexpr const char CalculatorEndPoint<T>::FILE_ROOT_DIRECTORY[];
 
 }}
 
--- CalculatorBackEnd/src/HttpServer/src/HttpRequestHandler.cpp
+++ CalculatorBackEnd/src/HttpServer/src/HttpRequestHandler.cpp
@@ -2,11 +2,81 @@
 #include <filesystem>
 
 #include <boost/utility/string_ref.hpp>
+#include <pistache/router.h>
 
+//#include "ExpressionParser.h"
+#include "ExpressionSerializer.h"
 #include "HttpRequestHandler.h"
 
 namespace calculator { namespace httpserver
 {
+// ---------------------------------------------------------------------------//
+// //
+// CalculatorEndPoint<T> Public Methods //
+// //
+// ---------------------------------------------------------------------------//
+template<typename T>
+CalculatorEndPoint<T>::CalculatorEndPoint(Pistache::Address address, size_t threads = 4) :
+        internalEndPoint_{std::make_shared<Pistache::Http::Endpoint>{address}}
+{
+    initializeRoutes();
+    initializeEndPoint(threads);
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::start()
+{
+    internalEndPoint_->serve();
+}
+
+// ---------------------------------------------------------------------------//
+// //
+// CalculatorEndPoint<T> Private Methods //
+// //
+// ---------------------------------------------------------------------------//
+template<typename T>
+void CalculatorEndPoint<T>::initializeRoutes()
+{
+    // API routes
+    Pistache::Rest::Routes::Get(router_, API_LIST_PREFIX, Pistache::Rest::Routes::bind(&CalculatorEndPoint<T>::handleApiList, this));
+    Pistache::Rest::Routes::Post(router_, API_STORE_PREFIX, Pistache::Rest::Routes::bind(&CalculatorEndPoint<T>::handleApiStore, this));
+    Pistache::Rest::Routes::Post(router_, API_CALCULATE_PREFIX, Pistache::Rest::Routes::bind(&CalculatorEndPoint<T>::handleApiCalculate, this));
+
+    // file resource routes
+    Pistache::Rest::Routes::Get(router_, FILE_RESOURCE_PREFIX, Pistache::Rest::Routes::bind(&CalculatorEndPoint<T>::handleFileResource, this));
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::initializeEndPoint(size_t threads)
+{
+    internalEndPoint_->init(Http::Endpoint::options().threads(static_cast<int>(threads)));
+    internalEndPoint_->setHandler(router_.handler());
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::handleApiStore(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
+{
+    std::lock_guard<std::mutex> guard{savedExpressionsLock_};
+    const std::string request.body();
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::handleApiList(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
+{
+
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::handleApiCalculate(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
+{
+
+}
+
+template<typename T>
+void CalculatorEndPoint<T>::handleFileResource(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response)
+{
+
+}
 
 void HttpRequestHandler::onRequest(const Pistache::Http::Request& req, Pistache::Http::ResponseWriter response)
 {
