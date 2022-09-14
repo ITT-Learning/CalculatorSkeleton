1b0dedd61f8ebb50ab34f4352aad87cdfc08b5d1
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-09-14 16:02:19 -0600

    operator unit tests

--- test/AdditionTests.cpp
+++ test/AdditionTests.cpp
@@ -7,14 +7,45 @@
 
 using namespace testing;
 
-class AdditionTests : public ::testing::Test{};
+class AdditionTests : public ::testing::Test {};
 
-TEST_F(AdditionTests, AddsNumbersAssociatively)
+
+
+TEST_F(AdditionTests, AddsPositiveNumbers)
 {
-    IMathOperation* constant14p4 = new Constant(14.4);
-    IMathOperation* constantN21p1 = new Constant(-21.1);
-    Addition sumOf14p4AndN21p1(constantN21p1, constant14p4);
-    Addition sumOfN21p1And14p4(constant14p4, constantN21p1);
-    EXPECT_DOUBLE_EQ(sumOf14p4AndN21p1.calculate(), -6.7);
-    EXPECT_DOUBLE_EQ(sumOfN21p1And14p4.calculate(), -6.7);
+    Addition sumOf10And15(new Constant(10), new Constant(15));
+    EXPECT_DOUBLE_EQ(sumOf10And15.calculate(), 25);
+};
+
+
+
+TEST_F(AdditionTests, AddsPositiveAndNegativeNumbers)
+{
+    Addition sumOfN10And15(new Constant(-10), new Constant(15));
+    EXPECT_DOUBLE_EQ(sumOfN10And15.calculate(), 5);
+};
+
+
+
+TEST_F(AdditionTests, AddsNegativeNumbers)
+{
+    Addition sumOfN10AndN15(new Constant(-10), new Constant(-15));
+    EXPECT_DOUBLE_EQ(sumOfN10AndN15.calculate(), -25);
+};
+
+
+
+TEST_F(AdditionTests, AddsFractionalNumbers)
+{
+    Addition sumOf1p5And1p3(new Constant(1.5), new Constant(1.3));
+    EXPECT_DOUBLE_EQ(sumOf1p5And1p3.calculate(), 2.8);
+};
+
+
+
+TEST_F(AdditionTests, AddsAssociativelyCorrectly)
+{
+    Addition sumOfN10And15(new Constant(-10), new Constant(15));
+    Addition sumOf15AndN10(new Constant(15), new Constant(-10));
+    EXPECT_DOUBLE_EQ(sumOf15AndN10.calculate(), sumOfN10And15.calculate());
 };
--- test/ConstantTests.cpp
+++ test/ConstantTests.cpp
@@ -5,11 +5,23 @@ using namespace testing;
 
 #include "Constant.h"
 
-class ConstantTests : public ::testing::Test{};
+class ConstantTests : public ::testing::Test {};
+
+
 
 TEST_F(ConstantTests, HoldsItsValue)
 {
     double value = 10.5;
     Constant constValue(value);
     EXPECT_DOUBLE_EQ(constValue.calculate(), value);
+};
+
+
+
+TEST_F(ConstantTests, TwoConstantsWithSameValueAreEqual)
+{
+    double value = 1;
+    Constant constant1(value);
+    Constant constant2(value);
+    EXPECT_DOUBLE_EQ(constant1.calculate(), constant2.calculate());
 };
