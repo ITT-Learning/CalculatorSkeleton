c9f00a11860930bf501c8f3a2e21172a8c773127
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-09-28 15:38:04 -0600

    removed unused function

--- inc/Calculator.h
+++ inc/Calculator.h
@@ -32,8 +32,6 @@ class Calculator
 
         Result<IMathOperation>                 extractOperation(std::stack<std::string>& postfixStack) const;
         static Result<std::stack<std::string>> infixToPostfix(std::vector<std::string> infixString);
-
-        static std::string extractNextNumberFromString(std::string str);
 };
 
 #endif
