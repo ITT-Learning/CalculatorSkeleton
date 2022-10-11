f04c5c0ee3b2a1b4d2efcd0b6c158995a5969b5d
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-10-11 16:33:20 -0600

    started the process of flatbuffer json parsing

--- src/Controllers/CalculatorController.cpp
+++ src/Controllers/CalculatorController.cpp
@@ -2,11 +2,12 @@
 
 #include <string>
 #include <unordered_map>
-#include <iostream>
 
 #include <pistache/router.h>
 #include <pistache/http.h>
-#include <rapidjson/document.h>
+#include <flatbuffers/flatbuffers.h>
+#include <flatbuffers/idl.h>
+#include <flatbuffers/util.h>
 
 #include "CalculatorService.h"
 #include "Result.h"
@@ -29,97 +30,61 @@ static void sendError(Pistache::Http::ResponseWriter& res, std::string message)
 
 void CalculatorController::calculate(const Pistache::Rest::Request& req, Pistache::Http::ResponseWriter res)
 {
-    std::string bodyString = req.body();
-    rapidjson::Document bodyJson;
-    bodyJson.Parse(bodyString.c_str());
-    if (!bodyJson.IsObject())
+    std::string equationSchema;
+    bool schemaLoaded = flatbuffers::LoadFile("./flatbuffers/Equation.fbs", false, &equationSchema);
+    if (!schemaLoaded)
     {
-        sendError(res, "Root value is not a valid object.");
+        res.send(
+            Pistache::Http::Code::Internal_Server_Error,
+            "Unable to initialize equation parsing",
+            Pistache::Http::Mime::MediaType(
+                Pistache::Http::Mime::Type::Text,
+                Pistache::Http::Mime::Subtype::Plain));
         return;
     }
 
-    if (!bodyJson.HasMember("equation"))
+    flatbuffers::Parser parser;
+    bool schemaParsed = parser.Parse(equationSchema.c_str());
+    if (!schemaParsed)
     {
-        sendError(res, "Request body should be a JSON object with an \"equation\" string.");
+        res.send(
+            Pistache::Http::Code::Internal_Server_Error,
+            parser.error_,
+            Pistache::Http::Mime::MediaType(
+                Pistache::Http::Mime::Type::Text,
+                Pistache::Http::Mime::Subtype::Plain));
         return;
     }
-    const rapidjson::Value& equationJson = bodyJson["equation"];
-    if (!equationJson.IsString())
+    bool jsonParsed = parser.Parse(req.body().c_str());
+    if (!jsonParsed)
     {
-        sendError(res, "Equation should be a string.");
-        return;
-    }
-    std::string equationString = equationJson.GetString();
-
-    std::unordered_map<std::string, double> variables;
-    if (bodyJson.HasMember("variables"))
-    {
-        const rapidjson::Value& variablesArray = bodyJson["variables"];
-        if (!variablesArray.IsArray())
-        {
-            sendError(res, "Variables should be an array.");
-            return;
-        }
-
-        for (rapidjson::Value::ConstValueIterator it = variablesArray.Begin(); it != variablesArray.End(); it++)
-        {
-            if (!it->IsObject())
-            {
-                sendError(res, "Items in the variables array must be objects.");
-                return;
-            }
-
-            rapidjson::GenericObject variableObject = it->GetObject();
-            if (!variableObject.HasMember("name"))
-            {
-                sendError(res, "A variable is missing a name.");
-                return;
-            }
-            const rapidjson::Value& variableName = variableObject["name"];
-            if (!variableName.IsString())
-            {
-                std::string message = "A variable has a non-string name. (";
-                message += variableName.GetString();
-                message += ")";
-
-                sendError(res, message);
-                return;
-            }
-            std::string name = variableName.GetString();
-
-            if (!variableObject.HasMember("value"))
-            {
-                std::string message = "Variable '" + name + "' does not have a value.";
-                sendError(res, message);
-                return;
-            }
-            const rapidjson::Value& variableValue = variableObject["value"];
-            if (!variableValue.IsNumber())
-            {
-                std::string message = "Variable '" + name + "' has a non-number value. (" + variableValue.GetString() + ")";
-                sendError(res, message);
-                return;
-            }
-            double value = variableValue.GetDouble();
-
-            variables.erase  (name);
-            variables.emplace(name, value);
-        }
-    }
-
-    Result<std::string> result = CalculatorService::calculate(equationString, variables);
-
-    if (!result.isValid())
-    {
-        sendError(res, result.getError());
+        res.send(
+            Pistache::Http::Code::Bad_Request,
+            parser.error_,
+            Pistache::Http::Mime::MediaType(
+                Pistache::Http::Mime::Type::Text,
+                Pistache::Http::Mime::Subtype::Plain));
         return;
     }
 
-    CalcHistoryPair newHistoryEntry(equationString, result.getResult());
-    HistoryService::addEntry(newHistoryEntry);
+    res.send(Pistache::Http::Code::Bad_Request, "// TODO finish building this out");
+    // std::string equationString = equationJson.GetString();
+
+    // std::unordered_map<std::string, double> variables;
+
+    // Result<std::string> result = CalculatorService::calculate(equationString, variables);
+
+    // if (!result.isValid())
+    // {
+    //     sendError(res, result.getError());
+    //     return;
+    // }
+
+    // CalcHistoryPair newHistoryEntry(equationString, result.getResult());
+    // HistoryService::addEntry(newHistoryEntry);
     
-    res.send(Pistache::Http::Code::Ok, result.getResult(), Pistache::Http::Mime::MediaType(
-                                                               Pistache::Http::Mime::Type::Text,
-                                                               Pistache::Http::Mime::Subtype::Plain));
-    return;
+    // res.send(Pistache::Http::Code::Ok, result.getResult(), Pistache::Http::Mime::MediaType(
+    //                                                            Pistache::Http::Mime::Type::Text,
+    //                                                            Pistache::Http::Mime::Subtype::Plain));
+    // return;
 };
